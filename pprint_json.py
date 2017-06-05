b7c7a83406c887376be6204d98bacf4bddd06b3a
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-06-05 11:19:53 +0200

    added file exists and its encoding checks

--- pprint_json.py
+++ pprint_json.py
@@ -1,19 +1,27 @@
 import json
 import codecs
 import sys
+import os
+import chardet
 
 
-def load_data(filename):
+def get_encoding(filename: "str") -> "str":
+    with open(filename, "rb") as file:
+        raw_data = file.read()
+    return chardet.detect(raw_data)['encoding']
+
+
+def load_data(filename: "str") -> "object json":
     try:
-        json_data = json.load(codecs.open(filename, 'r', 'utf-8-sig'))
+        json_data = json.load(codecs.open(filename, 'r', get_encoding(filename)))
     except json.decoder.JSONDecodeError:
-        print("invalid json format")
+        print("file {} contains invalid json data, print stopped".format(filename))
         exit(1)
     else:
         return json_data
 
 
-def pretty_print_json(json_data):
+def pretty_print_json(json_data: "object json"):
     print(json.dumps(json_data, sort_keys=True, indent=4, ensure_ascii=False))
 
 
@@ -22,4 +30,7 @@ def main(filename):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:][0])
+    if os.path.isfile(sys.argv[1:][0]):
+        main(sys.argv[1:][0])
+    else:
+        print("invalid path or file name: {}".format(sys.argv[1:][0]))
