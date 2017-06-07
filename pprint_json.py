818329bc4a510568af490857f7ef59c34134c3cb
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-06-07 20:11:58 +0200

    changed json.decode.error handling, argv[1:][0] changed to argv[1]

--- pprint_json.py
+++ pprint_json.py
@@ -4,23 +4,19 @@ import os
 import chardet
 
 
-def load_decoded_data_and_encoding(filename: "str") -> "tuple":
+def load_decoded_data(filename: "str") -> "str":
     with open(filename, "rb") as file:
         raw_data = file.read()
     encoding = chardet.detect(raw_data)['encoding']
-    return raw_data.decode(encoding), encoding
+    return raw_data.decode(encoding)
 
 
 def load_json_data(filename: "str") -> "dict":
-    decoded_data, _ = load_decoded_data_and_encoding(filename)
     try:
-        json_data = json.loads(decoded_data)
+        return json.loads(load_decoded_data(filename))
     except json.decoder.JSONDecodeError:
-        print("file {} contains invalid json data, load aborted!".format(filename))
-        exit(1)
-    else:
-        return json_data
-        
+        print("file {} contains invalid json data".format(filename))
+
 
 def pretty_print_json(json_data: "object json"):
     print(json.dumps(json_data, sort_keys=True, indent=4, ensure_ascii=False))
@@ -31,7 +27,7 @@ def main(filename):
 
 
 if __name__ == '__main__':
-    if os.path.isfile(sys.argv[1:][0]):
-        main(sys.argv[1:][0])
+    if os.path.isfile(sys.argv[1]):
+        main(sys.argv[1])
     else:
         print("invalid path or file name: {}".format(sys.argv[1:][0]))
